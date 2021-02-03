3b72c5fbdd5367f44525074c27dd65e3773377df
gkyglr39 <36515676+gkyglr39@users.noreply.github.com>
2021-02-03 23:47:12 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EFCategoryDal.cs
@@ -0,0 +1,42 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EFCategoryDal : ICategoryDal
+    {
+        public void Add(Category product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category product)
+        {
+            throw new NotImplementedException();
+        }
+
+        Category IEntityRepository<Category>.Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class NorthwindContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=GÖKAYGÜLER\SQLEXPRESS;database=Northwind;integrated security=true");
+        }
+    }
+}
